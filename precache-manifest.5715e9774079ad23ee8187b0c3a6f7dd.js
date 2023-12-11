3c665d711e0b064771b5ff871ac06e3826656c52
Devesh <devesh.kr.sri@gmail.com>
2023-12-11 11:49:22 +0000

    Updates

---
+++ precache-manifest.5715e9774079ad23ee8187b0c3a6f7dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b25d1f8e6b61d133d427d641289ce5",
+    "url": "/deveshprojectwork/index.html"
+  },
+  {
+    "revision": "599bcb5cbc3c87592a85",
+    "url": "/deveshprojectwork/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "2a11971225e7dcb80ac8",
+    "url": "/deveshprojectwork/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "599bcb5cbc3c87592a85",
+    "url": "/deveshprojectwork/static/js/2.1997bc18.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/deveshprojectwork/static/js/2.1997bc18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a11971225e7dcb80ac8",
+    "url": "/deveshprojectwork/static/js/main.8308218c.chunk.js"
+  },
+  {
+    "revision": "a6e92bcae81cd803ff6d",
+    "url": "/deveshprojectwork/static/js/runtime-main.1a10ceee.js"
+  }
+]);
