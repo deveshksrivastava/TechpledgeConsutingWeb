285fe1ba7c98c6bd81fa6be14f958037aa7d3e07
Devesh <devesh.kr.sri@gmail.com>
2023-12-11 07:54:19 +0000

    Fix version issue and migrated application to 2024

--- src/Components/CardCertificate.tsx
+++ src/Components/CardCertificate.tsx
@@ -27,67 +27,68 @@ export default function CardCertificate() {
 
 
   return (
+    <div>dfdf</div>
     
-      <section className="w3l-features-1">
-          <div className="features py-4">
-                  <div className="container pb-5">
-                      <div className="fea-gd-vv row ">	
-                        <div className="float-lt feature-gd col-lg-4 col-sm-12">	
-                          <div className="icon-info">
-                              <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
-                          </div>					 
-                        </div>	
-                        <div className="float-lt feature-gd col-lg-4 col-sm-12">	
-                          <div className="icon-info">
-                              <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
-                          </div>					 
-                        </div>	
-                        <div className="float-lt feature-gd col-lg-4 col-sm-12">	
-                          <div className="icon-info">
-                              <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
-                          </div>					 
-                        </div>	
-                      </div>  
-                      <hr />
-                      <div className="fea-gd-vv row ">	
-                        <div className="float-lt feature-gd col-lg-4 col-sm-12">	
-                          <div className="icon-info">
-                              <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
-                          </div>					 
-                        </div>	
-                        <div className="float-lt feature-gd col-lg-4 col-sm-12">	
-                          <div className="icon-info">
-                              <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
-                          </div>					 
-                        </div>	
-                        <div className="float-lt feature-gd col-lg-4 col-sm-12">	
-                          <div className="icon-info">
-                              <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
-                          </div>					 
-                        </div>	
-                      </div>  
-                      <hr/>
-                      <div className="fea-gd-vv row ">	
-                        <div className="float-lt feature-gd col-lg-4 col-sm-12">	
-                          <div className="icon-info">
-                               <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
-                          </div>					 
-                        </div>	
-                        <div className="float-lt feature-gd col-lg-4 col-sm-12">	
-                          <div className="icon-info">
-                          <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
-                          </div>					 
-                        </div>	
-                        <div className="float-lt feature-gd col-lg-4 col-sm-12">	
-                          <div className="icon-info">
-                          <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
-                          </div>					 
-                        </div>	
-                      </div>  
+    //   <section className="w3l-features-1">
+    //       <div className="features py-4">
+    //               <div className="container pb-5">
+    //                   <div className="fea-gd-vv row ">	
+    //                     <div className="float-lt feature-gd col-lg-4 col-sm-12">	
+    //                       <div className="icon-info">
+    //                           <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
+    //                       </div>					 
+    //                     </div>	
+    //                     <div className="float-lt feature-gd col-lg-4 col-sm-12">	
+    //                       <div className="icon-info">
+    //                           <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
+    //                       </div>					 
+    //                     </div>	
+    //                     <div className="float-lt feature-gd col-lg-4 col-sm-12">	
+    //                       <div className="icon-info">
+    //                           <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
+    //                       </div>					 
+    //                     </div>	
+    //                   </div>  
+    //                   <hr />
+    //                   <div className="fea-gd-vv row ">	
+    //                     <div className="float-lt feature-gd col-lg-4 col-sm-12">	
+    //                       <div className="icon-info">
+    //                           <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
+    //                       </div>					 
+    //                     </div>	
+    //                     <div className="float-lt feature-gd col-lg-4 col-sm-12">	
+    //                       <div className="icon-info">
+    //                           <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
+    //                       </div>					 
+    //                     </div>	
+    //                     <div className="float-lt feature-gd col-lg-4 col-sm-12">	
+    //                       <div className="icon-info">
+    //                           <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
+    //                       </div>					 
+    //                     </div>	
+    //                   </div>  
+    //                   <hr/>
+    //                   <div className="fea-gd-vv row ">	
+    //                     <div className="float-lt feature-gd col-lg-4 col-sm-12">	
+    //                       <div className="icon-info">
+    //                            <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
+    //                       </div>					 
+    //                     </div>	
+    //                     <div className="float-lt feature-gd col-lg-4 col-sm-12">	
+    //                       <div className="icon-info">
+    //                       <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
+    //                       </div>					 
+    //                     </div>	
+    //                     <div className="float-lt feature-gd col-lg-4 col-sm-12">	
+    //                       <div className="icon-info">
+    //                       <Cards title={`test`} certificate={`certificate`} examcode={`examcode`} tranhrs={`tranhrs`} exmpre={`exmpre`} price={`price`} pdfURL={`pdfURL`} />
+    //                       </div>					 
+    //                     </div>	
+    //                   </div>  
                       
-                  </div>
-          </div>
+    //               </div>
+    //       </div>
      
-     </section>
+    //  </section>
   );
 }
